35f346e1a7ca86e857e63e80399b00fe03823dbc
leotn03 <leo.tn03@icloud.com>
2023-12-30 09:14:24 -0500

    Initial Commit

---
+++ Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Leo Torres Neyra on 30/12/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
